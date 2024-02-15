dbfbe232b7a180fb36c8d60f673cffcd4b1570e0
nicomanunta <nic.manun@gmail.com>
2024-02-15 17:04:21 +0100

    aggiunto un dato manualmente

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -16,6 +16,31 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        $trains = 
+        $trains = [
+            [
+                'agency' => 'Trenitalia'
+                'departure_station' => 'Cagliari'
+                'arrival_station' => 'Macomer'
+                'departure_time' => '2024-02-15 18:00'
+                'arrival_time' => '2024-02-15 20:00'
+                'train_code' => '4758'
+                'number_of_carriages' => '10'
+                'in_time' => 'true'
+                'deleted' => ''
+            ]
+            ];
+            foreach($trains as $train){
+                $new_train = new Train();
+                $new_train ->agency = $train['agency'];
+                $new_train ->departure_station = $train['departure_station'];
+                $new_train ->arrival_station = $train['arrival_station'];
+                $new_train ->departure_time = $train['departure_time'];
+                $new_train ->arrival_time = $train['arrival_time'];
+                $new_train ->train_code = $train['train_code'];
+                $new_train ->number_of_carriages = $train['number_of_carriages'];
+                $new_train ->in_time = $train['in_time'];
+                $new_train ->deleted = $train['deleted'];
+                $new_train -> save();
+            }
     }
 }
