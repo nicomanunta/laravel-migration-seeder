c472e23b9568efd5aca0b2e6d4e5903964cb2a84
nicomanunta <nic.manun@gmail.com>
2024-02-15 16:33:05 +0100

    corretto problema, tabella trains creata

--- database/migrations/2024_02_15_150650_create_trains_table.php
+++ database/migrations/2024_02_15_150650_create_trains_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('agency', 50);
-            $table->string('departure_station');
-            $table->string('arrival_station');
-            $table->timestamp('departure_time');
-            $table->timestamp('arrival_time');
-            $table->string('train_code');
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->timestamp('departure_time')->nullable();
+            $table->timestamp('arrival_time')->nullable();
+            $table->string('train_code', 40);
             $table->integer('number_of_carriages');
             $table->boolean('in_time');
             $table->boolean('deleted')->default(false);
